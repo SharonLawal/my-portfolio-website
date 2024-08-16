492ff6cb4cf9d5229839d7f211475b4967a525f6
Sharon Lawal <sharonayolawal@gmail.com>
2024-08-16 18:44:30 +0100

    Added index page

---
+++ src/app/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Header />
+      <Footer />
+    </div>
+  );
+}
